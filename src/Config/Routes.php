6ed43fa9845da910d166a071eb11b27cebad4ab7
iqlearning <iqlearnacc@gmail.com>
2026-01-13 14:09:15 +0700

    add self routes

---
+++ src/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * Route collection for Pulse.
+ */
+return function (RouteCollection $routes) {
+    $routes->group('pulse', static function (RouteCollection $routes) {
+        $routes->get('/', '\Iqlearning\Pulse\Controllers\PulseController::index');
+        $routes->get('check', '\Iqlearning\Pulse\Controllers\PulseController::check'); // For Scheduled Tasks / Cron
+        $routes->get('stats', '\Iqlearning\Pulse\Controllers\PulseController::stats'); // Dashboard Data
+    });
+};
